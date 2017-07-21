78b1f2c2ff131c927f89e30c0a5fc7e11c994252
zhanghongpeng <13466555144@163.com>
2017-07-21 09:54:47 +0800

    spring上传文件demo

--- alan-springmvc-web/src/main/java/springData/controller/UserAction.java
+++ alan-springmvc-web/src/main/java/springData/controller/UserAction.java
@@ -1,5 +1,10 @@
 package springData.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -7,8 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
 import com.alan.springmvc.service.UserService;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 
@@ -26,7 +34,34 @@ public class UserAction {
     public String index(){
         return "index";
     }
+    @RequestMapping("/upload")
+    public String toPage(){
+        return "upload";
+    }
     
+    /**
+     * method1:通过参数CommonsMultipartFile进行解析
+     * @RequestParam("file")中的file对应于upload.jsp中的file类型的name对应的名称
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value="/uploadfile")
+    public String upload1(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        //获取文件名称
+        String fileName = file.getOriginalFilename();
+        //写入本地磁盘
+        InputStream is = file.getInputStream();
+        byte[] bs = new byte[1024];
+        int len;
+        OutputStream os = new FileOutputStream(new File("/Users/zxc/Desktop/" + fileName));
+        while ((len = is.read(bs)) != -1) {
+            os.write(bs, 0, len);
+        }
+        os.close();
+        is.close();
+        return "upload_success";
+    }
     @RequestMapping("/info")
     public String info(){
         return "info";
@@ -58,13 +93,13 @@ public class UserAction {
     
     @RequestMapping("/findbyss")
     @ResponseBody
-    public Hentity findByss(Integer id){
+    public Map findByss(Integer id){
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("state", "success");
         String url = "http://taccount.haier.com/oauth/token?client_id=haokongqi&client_secret=h1otB90c6Am_e9&grant_type=client_credentials";
-        Hentity hentity = restTemplate.postForObject(url, null,Hentity.class);
-        log.info(hentity.getAccess_token()+"====="+hentity.getExpires_in());
-        return hentity;
+        Map map1 = restTemplate.postForObject(url, null,HashMap.class);
+        log.info(map1.toString());
+        return map1;
     }
     
     
